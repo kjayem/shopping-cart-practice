e98c8a47b8ee688ea3cced86cf5cb1c6d8d1b95a
kjayem <flwfeeld@gmail.com>
2021-08-22 15:12:18 +0900

    added navbar from bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var expressHbs = require('express-handlebars');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -9,8 +12,11 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({
+  defaultLayout: 'layout', 
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
